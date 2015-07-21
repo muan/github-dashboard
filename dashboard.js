cbfce08a5f420da63573394314b09656feeec232
Mu-An Chiou <me@muanchiou.com>
2015-07-22 06:16:48 +0800

    House keeping

--- dashboard.js
+++ dashboard.js
@@ -1,37 +1,60 @@
-$(document).ready(function() {
+$(document).ready(function () {
   init()
 })
 
-$(document).on("change", ".filter-checkbox", function() {
+$(document).on('change', '.filter-checkbox', function () {
   rememberPreference()
 })
 
-var events = ["Stars", "Forks", "Comments", "Repositories", "Issues", "Org"]
+var events = ['Stars', 'Forks', 'Comments', 'Repositories', 'Issues', 'Org']
 
-function init() {
-  events.map(function(key) {
-    $(".news").prepend("<input type=checkbox id=\""+ key + "\" class=\"filter-checkbox filtered-"+ key.toLowerCase() +"\"> <label class=\"filter-label\" for=\""+ key + "\">"+ key + "</label>")
+function init () {
+  events.map(function (key) {
+    $('.account-switcher').after('<input type=checkbox id=\'' + key + '\' class=\'filter-checkbox filtered-' + key.toLowerCase() + '\'> <label class=\'filter-label\' for=\'' + key + '\'>' + key + '</label>')
   })
   applyPreference()
 }
 
-function rememberPreference() {
+function rememberPreference () {
   var preference = {}
-  $('.filter-checkbox').each(function(_, box) {
-    preference[box.id] = $(box).is(":checked")
+  $('.filter-checkbox').each(function (_, box) {
+    preference[box.id] = $(box).is(':checked')
   })
-  localStorage.setItem("dashboard:select", JSON.stringify(preference))
+  localStorage.setItem('dashboard:select', JSON.stringify(preference))
+  loadMoreItemsIfApplicable()
 }
 
-function applyPreference() {
-  var preference = localStorage.getItem("dashboard:select")
-  if(preference) {
+function applyPreference () {
+  var preference = localStorage.getItem('dashboard:select')
+  if (preference) {
     preference = JSON.parse(preference)
 
-    $(".filter-checkbox").each(function(_, box) {
-      $(box).prop("checked", preference[box.id])
+    $('.filter-checkbox').each(function (_, box) {
+      $(box).prop('checked', preference[box.id])
     })
   } else {
-    $(".filter-checkbox").prop("checked", true)
+    $('.filter-checkbox').prop('checked', true)
+  }
+
+  loadMoreItemsIfApplicable()
+}
+
+function loadMoreItemsIfApplicable () {
+  var paginate = $('.ajax_paginate')
+  var paginateBtn = $('.js-events-pagination')
+  if (paginateBtn.length === 0) { return false }
+
+  var visibleItems = Array.prototype.filter.call(document.getElementsByClassName('alert'), function (ele) {
+    return ele.offsetHeight > 0
+  })
+
+  if (visibleItems.length < 10) {
+    $.ajax(paginateBtn.attr('href')).done(function (data) {
+      paginate.replaceWith(data)
+
+      if (visibleItems.length < 10) {
+        loadMoreItemsIfApplicable()
+      }
+    })
   }
 }
